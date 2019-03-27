c489bf35878934c357a8af7fbb5cc37838ddfcdc
K0170016 <K0170016@test.htsc.com.cn>
2019-03-27 17:10:28 +0800

    代码规范

---
+++ module_wan/src/test/java/com/huyingbao/module/wan/ui/article/action/ArticleActionTest.java
@@ -0,0 +1,23 @@
+package com.huyingbao.module.wan.ui.article.action;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by liujunfeng on 2019/3/27.
+ */
+public class ArticleActionTest {
+    @Rule
+    private Timeout mTimeout = new Timeout(1000, TimeUnit.DAYS);
+
+    @Test
+    public void getArticleList() {
+    }
+
+    @Test
+    public void getBannerList() {
+    }
+}
