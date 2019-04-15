a07f704cad53795749e904e5ed0d7c492cf16d69
K0170016 <K0170016@test.htsc.com.cn>
2019-04-15 16:13:47 +0800

    代码规范

--- module_app/src/app/java/com/huyingbao/module/app/SimpleApplication.java
+++ module_app/src/app/java/com/huyingbao/module/app/SimpleApplication.java
@@ -2,6 +2,10 @@ package com.huyingbao.module.app;
 
 
 import com.huyingbao.core.base.BaseApp;
+import com.huyingbao.module.gan.GanEventBusIndex;
+import com.huyingbao.module.wan.WanEventBusIndex;
+
+import org.greenrobot.eventbus.EventBus;
 
 import dagger.android.AndroidInjector;
 import dagger.android.support.DaggerApplication;
@@ -15,5 +19,15 @@ public class SimpleApplication extends BaseApp {
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
         return DaggerSimpleComponent.builder().application(this).build();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        EventBus.builder()
+                .addIndex(new WanEventBusIndex())
+                .addIndex(new GanEventBusIndex())
+                .eventInheritance(false)
+                .installDefaultEventBus();
     }
 }
