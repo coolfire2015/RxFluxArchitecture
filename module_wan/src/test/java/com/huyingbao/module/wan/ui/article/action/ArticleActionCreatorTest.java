c860d731cb21b8c6fd7c3b55d49e1e1b7e37a2ef
K0170016 <K0170016@test.htsc.com.cn>
2019-04-18 18:31:44 +0800

    代码规范

--- module_wan/src/test/java/com/huyingbao/module/wan/ui/article/action/ArticleActionCreatorTest.java
+++ module_wan/src/test/java/com/huyingbao/module/wan/ui/article/action/ArticleActionCreatorTest.java
@@ -2,11 +2,13 @@ package com.huyingbao.module.wan.ui.article.action;
 
 import com.huyingbao.core.arch.action.RxActionManager;
 import com.huyingbao.core.arch.dispatcher.RxDispatcher;
+import com.huyingbao.core.arch.model.RxAction;
 import com.huyingbao.module.wan.module.MockDaggerRule;
 import com.huyingbao.module.wan.module.MockUtils;
 import com.huyingbao.module.wan.ui.article.store.ArticleStore;
 import com.huyingbao.test.junit.RxJavaRule;
 
+import org.greenrobot.eventbus.EventBus;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,7 +17,6 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 /**
@@ -37,20 +38,26 @@ public class ArticleActionCreatorTest {
     public MockDaggerRule mMockDaggerRule = new MockDaggerRule();
 
     private ArticleActionCreator mActionCreator;
+    private EventBus mEventBus = new EventBus();
 
     @Before
     public void setUp() {
         mActionCreator = new ArticleActionCreator(mRxDispatcher, mRxActionManager, MockUtils.getComponent().getWanApi());
-        mRxDispatcher.subscribeRxStore(mArticleStore);
+//        mRxDispatcher.subscribeRxStore(mArticleStore);
     }
 
     @Test
     public void testGetArticleList() {
-        mActionCreator.getArticleList(1);
-        //调用方法成功,发送一次RxAction
-        verify(mRxDispatcher).postRxAction(any());
-        //调用方法成功,发送两次RxLoading
-        verify(mRxDispatcher, times(2)).postRxLoading(any());
+//        mActionCreator.getArticleList(1);
+//        //调用方法成功,发送一次RxAction
+//        verify(mRxDispatcher).postRxAction(any());
+//        //调用方法成功,发送两次RxLoading
+//        verify(mRxDispatcher, times(2)).postRxLoading(any());
+
+        RxAction actionBuilder = new RxAction.Builder(ArticleAction.GET_ARTICLE_LIST).build();
+        mEventBus.register(mArticleStore);
+        mEventBus.post(actionBuilder,actionBuilder.getTag());
+//        mRxDispatcher.postRxAction(actionBuilder);
         verify(mArticleStore).setArticleLiveData(any());
     }
 
