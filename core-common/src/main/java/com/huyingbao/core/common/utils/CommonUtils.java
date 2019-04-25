b15e9806da8099b97c5b951f7781cb1ca70be60c
K0170016 <K0170016@test.htsc.com.cn>
2019-04-25 14:55:42 +0800

    common库拆分

--- core-common/src/main/java/com/huyingbao/core/common/utils/CommonUtils.java
+++ core-common/src/main/java/com/huyingbao/core/common/utils/CommonUtils.java
@@ -3,35 +3,10 @@ package com.huyingbao.core.common.utils;
 import android.content.Context;
 import android.widget.Toast;
 
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-
 /**
  * Created by liujunfeng on 2019/1/1.
  */
 public class CommonUtils {
-
-    /**
-     * 得到类的泛型类型
-     *
-     * @param aClass
-     * @param <T>
-     * @return
-     */
-    public static <T> Class<T> getGenericClass(Class aClass) {
-        Class<T> genericClass = null;
-        //返回直接继承的父类（包含泛型参数）
-        Type genericSuperclass = aClass.getGenericSuperclass();
-        if (genericSuperclass instanceof ParameterizedType) {
-            try {
-                genericClass = (Class<T>) ((ParameterizedType) genericSuperclass).getActualTypeArguments()[0];
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return genericClass;
-    }
-
     /**
      * 显示短暂的Toast
      *
