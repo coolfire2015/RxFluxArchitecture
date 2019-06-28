7afbddede6e54d026561558271093c8e04b61fe4
K0170016 <K0170016@test.htsc.com.cn>
2019-06-28 13:20:36 +0800

    单元测试Manifest支持

--- module-github/src/test/java/com/huyingbao/module/github/ui/login/view/LoginActivityTest.kt
+++ module-github/src/test/java/com/huyingbao/module/github/ui/login/view/LoginActivityTest.kt
@@ -4,16 +4,19 @@ import androidx.lifecycle.Lifecycle
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.huyingbao.module.github.GithubApplication
 
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
 /**
  * Created by liujunfeng on 2019/3/28.
  */
 @LargeTest
 @RunWith(AndroidJUnit4::class)
+@Config(application = GithubApplication::class)
 class LoginActivityTest {
     @get:Rule
     var activityRule: ActivityScenarioRule<LoginActivity> = ActivityScenarioRule(LoginActivity::class.java)
--- module-github/src/test/java/com/huyingbao/module/github/ui/login/view/LoginFragmentTest.kt
+++ module-github/src/test/java/com/huyingbao/module/github/ui/login/view/LoginFragmentTest.kt
@@ -6,18 +6,21 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.huyingbao.module.github.GithubApplication
 import com.huyingbao.module.github.R
 import com.huyingbao.test.utils.FragmentScenarioRule
 import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
 
 /**
  * Created by liujunfeng on 2019/3/28.
  */
 @RunWith(AndroidJUnit4::class)
+@Config(application = GithubApplication::class)
 class LoginFragmentTest {
     @get:Rule
     var scenarioRule: FragmentScenarioRule<LoginActivity, LoginFragment> = FragmentScenarioRule(
