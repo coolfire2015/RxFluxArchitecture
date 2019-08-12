3b9f33d2340fb0783c9f63e0f98313ee50534649
K0170016 <K0170016@test.htsc.com.cn>
2019-08-12 19:30:46 +0800

    架构调整

--- module-app/src/app/java/com/huyingbao/module/app/SimpleApplication.kt
+++ module-app/src/app/java/com/huyingbao/module/app/SimpleApplication.kt
@@ -1,16 +1,21 @@
-package com.huyingbao.module.common
+package com.huyingbao.module.app
+
 
 import com.huyingbao.core.annotations.RxAppOwner
 import com.huyingbao.core.base.BaseApp
+
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
 /**
+ * 主Application
+ *
+ *
  * Created by liujunfeng on 2019/1/1.
  */
 @RxAppOwner
-class CommonApplication : BaseApp() {
+class SimpleApplication : BaseApp() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-        return DaggerCommonComponent.builder().application(this).build()
+        return DaggerSimpleComponent.builder().application(this).build()
     }
 }
--- module-app/src/app/java/com/huyingbao/module/app/SimpleComponent.kt
+++ module-app/src/app/java/com/huyingbao/module/app/SimpleComponent.kt
@@ -1,46 +1,51 @@
-package com.huyingbao.module.app;
+package com.huyingbao.module.app
 
-import android.app.Application;
+import android.app.Application
 
-import com.huyingbao.core.common.module.CommonModule;
+import com.huyingbao.core.common.module.CommonModule
 
-import javax.inject.Singleton;
+import javax.inject.Singleton
 
-import dagger.BindsInstance;
-import dagger.Component;
-import dagger.android.AndroidInjectionModule;
-import dagger.android.AndroidInjector;
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import dagger.android.AndroidInjector
 
 /**
  * 主Module
- * <p>
- * 在AppComponent中将dagger2库里的{@link AndroidInjectionModule}注入到Application中，
+ *
+ *
+ * 在AppComponent中将dagger2库里的[AndroidInjectionModule]注入到Application中，
  * 并将Application实现相应的接口（例如：HasActivityInjector、HasFragmentInjector、
  * HasServiceInjector、HasBroadcastReceiverInjector等等很多），
  * 并返回相应的方法，返回值参照以上App中的方式；
- * <p>
+ *
+ *
  * 注射器AndroidInjector子类
  * 该类是接口或抽象类。它在编译时会产生相应的类的实例来作为提供依赖方和需要依赖方之间的桥梁。
- * <p>
+ *
+ *
  * Component需要引用到目标类的实例，
  * Component会查找目标类中用Inject注解标注的属性，
  * 查找到相应的属性后会接着查找该属性对应的用Inject标注的构造函数（这时候就发生联系了），
  * 剩下的工作就是初始化该属性的实例并把实例进行赋值。
  * 因此我们也可以给Component叫另外一个名字注入器Injector，
  * 事实上Component必须继承的父类就叫做AndroidInjector
- * <p>
+ *
+ *
  * Created by liujunfeng on 2019/1/1.
  */
 @Singleton
-@Component(modules = {
-        CommonModule.class,//通用module
-        AndroidInjectionModule.class})
-public interface SimpleComponent extends AndroidInjector<SimpleApplication> {
+@Component(modules = [
+    CommonModule::class, //通用module
+    AndroidInjectionModule::class
+])
+interface SimpleComponent : AndroidInjector<SimpleApplication> {
     @Component.Builder
     interface Builder {
         @BindsInstance
-        SimpleComponent.Builder application(Application application);
+        fun application(application: Application): SimpleComponent.Builder
 
-        SimpleComponent build();
+        fun build(): SimpleComponent
     }
 }
